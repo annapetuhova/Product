ea66c0ccfb9a9d02f76aa9e435e425971f9e4b69
annapetuhova <cosmostar@mail.ru>
2015-03-25 20:57:03 +0500

    pr
    product

---
+++ Product/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab7_ASP_Route.MvcApplication" Language="C#" %>
---
+++ Product/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab7_ASP_Route.Startup))]
+namespace lab7_ASP_Route
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
